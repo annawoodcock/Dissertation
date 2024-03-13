86ae006e22d7f7afcccef2d9cf8897752318c4d1
annawoodcock <72135233+annawoodcock@users.noreply.github.com>
2024-03-13 11:51:46 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,6 +10,15 @@ def load_diabetes_data_5050():
     data = (data - data.mean()) / data.std()
     x = torch.tensor(data.values).float()
     return train_test_split(x, y, test_size=0.2, random_state=73)
+    
+def load_diabetes_data_subset_1000():
+    data = pd.read_csv("diabetes_subset_1000.csv")
+    data = data.dropna()
+    y = torch.tensor(data["Diabetes_binary"].values).float().unsqueeze(1)
+    data = data.drop(columns=['Diabetes_binary'])
+    data = (data - data.mean()) / data.std()
+    x = torch.tensor(data.values).float()
+    return train_test_split(x, y, test_size=0.2, random_state=73)
 
 def print_metrics(accuracy, precision, recall, f1, confusion):
     print(f'Accuracy: {accuracy:.4f}')
@@ -17,3 +26,8 @@ def print_metrics(accuracy, precision, recall, f1, confusion):
     print(f'Recall: {recall:.4f}')
     print(f'F1 Score: {f1:.4f}')
     print(f'Confusion Matrix:\n {confusion}')
+
+def create_dataloader(x, y, batch_size=10):
+    ds = torch.utils.data.TensorDataset(x, y)
+    dl = torch.utils.data.DataLoader(ds, batch_size=batch_size)
+    return dl
